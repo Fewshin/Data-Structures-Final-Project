76f1501493394cafe9a6adce77ceb7cc26ed4081
bronto <reidstrieker869@gmail.com>
2020-04-26 16:42:18 -0600

    adding the code to calculate the average time for BST

--- driver.cpp
+++ driver.cpp
@@ -8,6 +8,7 @@
 #include <time.h>
 #include <vector>
 #include <fstream>
+#include <cstdlib>
 
 long * getData (char letter) {
   std::string defaultName = "dataSet";
@@ -30,4 +31,40 @@ int main () {
   E = End
   D = Difference/Time taken*/
 
+  // S
+  long testData[] = getData('A');
+  BST bst = BST();
+
+  float insert[400];
+  float search[400];
+  clock_t start, end;
+
+  // C
+  for(int i = 0; i < 400; i++) {
+    start = clock();
+    for(int j = (i * 100); j < (100 + i * 100); j++) {
+      bst.addNode((int) testData[j]);
+    }
+    end = clock();
+    // D
+    float avgTime = float(end - start) / (float(CLOCKS_PER_SEC) * 100);
+    insert[i] = avgTime;
+  }
+
+  // Z
+  for(int i = 0; i <= 40000; i = i + 100) {
+    int set[100];
+    for(int j = 0; j < 100; j++)
+      set[j] = rand() % (i + 100);
+    start = clock();
+    for(int k = 0; k < 100; k++) {
+      bst.searchKey(set[j]);
+    }
+    end = clock();
+    // D
+    float avgTime = float(end - start) / (float(CLOCKS_PER_SEC) * 100);
+    search[i / 100] = avgTime;
+  }
+
+  // E
 }
