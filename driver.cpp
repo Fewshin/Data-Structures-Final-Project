1f0ed29c45040f0d1a7e37473c4436a867960938
Fewshin <zorkcraft@gmail.com>
2020-04-26 19:49:06 -0500

    Pushing working getData function

--- driver.cpp
+++ driver.cpp
@@ -9,6 +9,7 @@
 #include <vector>
 #include <fstream>
 #include <cstdlib>
+#include <string>
 
 long * getData (char letter) {
   std::string defaultName = "dataSet";
@@ -19,52 +20,66 @@ long * getData (char letter) {
   /*Objective: Pull all long ints from the .csv file
   Push them into the vector, then return the address
   of the first value in the vector*/
+  std::string line;
+  std::getline(data, line); 
+  //printf("%s\n",line.c_str());//Holy hell, I just maxed my CPU printing this.
+  std::string temp = "";
+  //int count = 0;
+  for (int i = 0; i < line.length(); i++) {
+    if (line[i] != ',') {
+      temp.push_back(line[i]);
+    }
+    if (line[i] == ',') {
+      store.push_back(stoi(temp));
+      //printf(temp.c_str());
+      //count++;
+      temp = "";
+    }
+  }
+  store.push_back(stoi(temp));
+  //count++;
+  //printf("|%s\n%d\n",temp.c_str(), count);//Final count is 40,000 as expected.
+  //printf("%d\n", (int) store.size());//40,000 again, seems to be working as intended.
   return &store[0];
 }
 
 int main () {
   printf("Testing speed of table creation for BST\n");
-  /*Key:
+  /*Key for timing variable names:
   S = Start
   C = Creating Table
   Z = Searching Table
   E = End
   D = Difference/Time taken*/
 
-  // S
-  long testData[] = getData('A');
+  long * testData = getData('A');
   BST bst = BST();
 
   float insert[400];
   float search[400];
   clock_t start, end;
 
-  // C
   for(int i = 0; i < 400; i++) {
     start = clock();
     for(int j = (i * 100); j < (100 + i * 100); j++) {
       bst.addNode((int) testData[j]);
     }
     end = clock();
-    // D
     float avgTime = float(end - start) / (float(CLOCKS_PER_SEC) * 100);
     insert[i] = avgTime;
   }
 
-  // Z
   for(int i = 0; i <= 40000; i = i + 100) {
     int set[100];
     for(int j = 0; j < 100; j++)
       set[j] = rand() % (i + 100);
     start = clock();
     for(int k = 0; k < 100; k++) {
-      bst.searchKey(set[j]);
+      bst.searchKey(set[k]);
     }
     end = clock();
-    // D
     float avgTime = float(end - start) / (float(CLOCKS_PER_SEC) * 100);
     search[i / 100] = avgTime;
   }
 
-  // E
 }
