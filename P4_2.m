ac83a6d067143f3dd9ff1535a73e75cc8c09f0ae
bronto <reidstrieker869@gmail.com>
2020-04-28 17:57:38 -0600

    committing to pull changes

--- P4_2.m
+++
@@ -1,9 +0,0 @@
-SetA = importdata("dataSetA.csv");
-SetB = importdata("dataSetB.csv");
-
-figure(1)
-plot(SetA)
-title("Plot of points in Dataset A")
-figure(2)
-plot(SetB)
-title("Plot of points in Dataset B")
