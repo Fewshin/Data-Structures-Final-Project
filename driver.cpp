402e7026b0ec2f12db950e804420c15c10fcccd9
bronto <reidstrieker869@gmail.com>
2020-04-29 01:08:20 -0600

    adding driver code for BST and LL on dataSetB

--- driver.cpp
+++ driver.cpp
@@ -34,19 +34,19 @@ vector<long> getData (char letter) {
 }
 
 int main () {
-  printf("Testing speed of table creation for BST\n");
+  printf("Testing speed of table A creation for BST\n");
   vector<long> _testData = getData('A');
   long * testData = &_testData[0];
   int testDataSize = _testData.size();
   printf("%ld\n", testData[0]);
   printf("%d\n", testDataSize);
 
-  BST bst = BST();
+  BST bst_A = BST();
 
   // float * insert = &bst.createTree(testData)[0];
   // float * search = &bst.searchTree(testData)[0];
-  vector<float> insert = bst.createTree(testData);
-  vector<float> search = bst.searchTree(testData);
+  vector<float> insert = bst_A.createTree(testData);
+  vector<float> search = bst_A.searchTree(testData);
 
   ofstream insertData;
   ofstream searchData;
@@ -59,12 +59,12 @@ int main () {
   insertData.close();
   searchData.close();
 
-  printf("Testing speed of table creation for linked list\n");
+  printf("Testing speed of table A creation for linked list\n");
 
-  LinkedList ll = LinkedList();
+  LinkedList ll_A = LinkedList();
 
-  insert = ll.createList(testData);
-  search = ll.searchList(testData);
+  insert = ll_A.createList(testData);
+  search = ll_A.searchList(testData);
 
   insertData.open("insert_performance_LL_dataSetA.csv", std::fstream::in | std::fstream::out | std::fstream::app);
   searchData.open("search_performance_LL_dataSetA.csv", std::fstream::in | std::fstream::out | std::fstream::app);
@@ -75,12 +75,45 @@ int main () {
   insertData.close();
   searchData.close();
 
-
-  
   _testData.clear();
   _testData = getData('B');
   testData = &_testData[0];
   testDataSize = _testData.size();
+
+  printf("Testing speed of table B creation for BST\n");
+
+  BST bst_B = BST();
+
+  insert = bst_B.createTree(testData);
+  search = bst_B.searchTree(testData);
+
+  insertData.open("insert_performance_BST_dataSetB.csv", std::fstream::in | std::fstream::out | std::fstream::app);
+  searchData.open("search_performance_BST_dataSetB.csv", std::fstream::in | std::fstream::out | std::fstream::app);
+  for(int i = 0; i < 400; i++) {
+    insertData << insert[i] << "\n";
+    searchData << search[i] << "\n";
+  }
+  insertData.close();
+  searchData.close();
+
+  printf("Testing speed of table B creation for linked list\n");
+
+  LinkedList ll_B = LinkedList();
+
+  insert = ll_B.createList(testData);
+  search = ll_B.searchList(testData);
+
+  insertData.open("insert_performance_LL_dataSetB.csv", std::fstream::in | std::fstream::out | std::fstream::app);
+  searchData.open("search_performance_LL_dataSetB.csv", std::fstream::in | std::fstream::out | std::fstream::app);
+  for(int i = 0; i < 400; i++) {
+    insertData << insert[i] << "\n";
+    searchData << search[i] << "\n";
+  }
+  insertData.close();
+  searchData.close();
+
+
+
   //Code Here
   _testData.clear();
 }
