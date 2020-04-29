0908bdd40fe59860efac91f9bb21728cf0ad00f0
Fewshin <zorkcraft@gmail.com>
2020-04-29 17:09:33 -0500

    Hey! I'm applying for a "New Villain" loan, go by the name of Vector, it's a mathematical term, a quantity represented by an arrow, with both direction and magnitude. Vector! That's me! Cause I'm committing crimes with both direction, and magnitude! Oh Yeah!

--- ChainHashTable.cpp
+++ ChainHashTable.cpp
@@ -18,8 +18,11 @@ void ChainHashTable::insert(int key, int trialNum) {
   check->key = key;
 }
 
-int ChainHashTable::search(int key) {
-    return key % *tableSize;
+int ChainHashTable::search(int key, int trialNum) {
+  if (key != hashTable[key % *tableSize].key) {
+    searchCollisionCounts[trialNum]++;
+  }
+  return key % *tableSize;
 }
 
 vector<float> ChainHashTable::createTable(int size, long * input) {
@@ -36,6 +39,24 @@ vector<float> ChainHashTable::createTable(int size, long * input) {
       store.push_back((float) chrono::duration_cast<chrono::nanoseconds>((eTime - sTime) / 100).count());
       trialNum++;
       createCollisionCounts.push_back(0);
+      sTime = chrono::steady_clock::now();
+    }
+  }
+  return store;
+}
+
+vector<float> ChainHashTable::searchTable(int size, long * input) {
+  vector<float> store;
+  int trialNum = 0;
+  searchCollisionCounts.push_back(0);
+  auto sTime = chrono::steady_clock::now();
+  for (int i = 0; i < size; i++) {
+    search(input[i], trialNum);
+    if (i%100 == 0) {
+      auto eTime = chrono::steady_clock::now();
+      store.push_back((float) chrono::duration_cast<chrono::nanoseconds>((eTime - sTime) / 100).count());
+      trialNum++;
+      searchCollisionCounts.push_back(0);
       sTime = chrono::steady_clock::now();
     }
   }
--- ChainHashTable.hpp
+++ ChainHashTable.hpp
@@ -13,9 +13,11 @@ class ChainHashTable {
         int * tableSize;
         cHashNode * hashTable;
         vector<int> createCollisionCounts;
+        vector<int> searchCollisionCounts;
     public:
         ChainHashTable();
         void insert(int key, int trialNum);
-        int search(int key);
+        int search(int key, int trialNum);
         vector<float> createTable(int size, long * input);
+        vector<float> searchTable(int size, long * input);
 };
