93f036aed3ad567f064a364d48a61a0e77c8bfea
Fewshin <zorkcraft@gmail.com>
2020-04-29 16:42:30 -0500

    We accidently deleted the matlab file

---
+++ p4_2.m
@@ -0,0 +1,9 @@
+SetA = importdata("dataSetA.csv");
+SetB = importdata("dataSetB.csv");
+
+figure(1)
+plot(SetA)
+title("Plot of points in Dataset A")
+figure(2)
+plot(SetB)
+title("Plot of points in Dataset B")
