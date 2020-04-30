583842ffab761d1840245c1dbe141a3a22b52cae
Fewshin <zorkcraft@gmail.com>
2020-04-29 20:28:55 -0500

    Making a folder for .pngs, updating readme, and pushing matlab code

--- p4_2.m
+++ p4_2.m
@@ -2,8 +2,22 @@ SetA = importdata("dataSetA.csv");
 SetB = importdata("dataSetB.csv");
 
 figure(1)
-plot(SetA)
+plot(SetA, 'blue')
 title("Plot of points in Dataset A")
 figure(2)
-plot(SetB)
+plot(SetB, 'red')
 title("Plot of points in Dataset B")
+
+IPLLA = importdata("insert_performance_LL_dataSetA.csv");
+SPLLA = importdata("search_performance_LL_dataSetA.csv");
+
+IPLLB = importdata("insert_performance_LL_dataSetB.csv");
+SPLLB = importdata("search_performance_LL_dataSetB.csv");
+
+figure(3)
+hold on
+plot(IPLLA, 'blue')
+plot(IPLLB, 'red')
+title("Plot of insert performance for Linked Lists")
+ylabel("Time in nanoseconds")
+legend("Set A", "Set B")
